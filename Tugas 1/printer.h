9c1db268843795f2ca9b0c93b274212737a85e94
rionaldichandraseta <rionaldi.chandraseta@gmail.com>
2018-01-21 10:38:20 +0700

    Moved dictionary to exernal variable in printer.h

--- Tugas 1/printer.h
+++ Tugas 1/printer.h
@@ -3,7 +3,11 @@
 
 #include "framebuffer.h"
 
-void bufferChar(FrameBuffer* fb, char* c, int char_width, int char_height, int x, int y, int scale, int r, int g, int b, int a);
+extern char* cdict[255];
+
+void initializeDictionary();
+
+void bufferChar(FrameBuffer* fb, char c, int char_width, int char_height, int x, int y, int scale, int r, int g, int b, int a);
 
 void bufferString(FrameBuffer* fb, char* s, int char_width, int char_height, int x, int y, int spacing, int scale, int r, int g, int b, int a);
 
