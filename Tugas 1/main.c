2392b92c3916d0cdc67c9d87eb24d61542156dda
rionaldichandraseta <rionaldi.chandraseta@gmail.com>
2018-01-22 20:25:10 +0700

    Modified font printer, added line drawer

--- Tugas 1/main.c
+++ Tugas 1/main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #include "framebuffer.h"
+#include "drawer.h"
 #include "printer.h"
 
 /** 
@@ -23,16 +24,23 @@ int main(){
     fb = initialize();
     printf("Framebuffer initialized\n");
 
-    // char* font;
-    // printf("Font type: ");
-    // scanf("%s", font);
-    initializeDictionary("archaic");
+    // // char* font;
+    // // printf("Font type: ");
+    // // scanf("%s", font);
+    // initializeDictionary("archaic");
 
-    char* in;
-    printf("Input: ");
-    scanf("%s", in);
-    
-    bufferString(&fb, in, 9, 11, 1500, 300, 1, 3, 255, 255, 255, 0);
+    // char in[1000];
+    // printf("Input: ");
+    // scanf("%999[0-9a-zA-Z ]", in);
+    // printf("%s",in);
+    // bufferString(&fb, in, 9, 11, 1500, 300, 1, 3, 255, 255, 255, 0);
+
+    drawLine(&fb, 400, 600, 400, 800, 255, 255, 255, 0);
+    drawCircle(&fb, 400, 600, 200, 255, 255, 255, 0);
+
+    for (i=1; i<=200; i++) {
+        drawCircle(&fb, 800, 600, i, 255, 255, 255, 0);
+    }
 
     updateFrame(&fb);
     
