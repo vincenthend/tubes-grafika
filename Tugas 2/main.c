e5afb23f0aa67aa4d120734c2321a4feff6be0a8
agusgun <agoundragon@gmail.com>
2018-01-28 21:15:50 +0700

    Changed background color and added laser

--- Tugas 2/main.c
+++ Tugas 2/main.c
@@ -34,13 +34,19 @@ int main(){
 
     //Initialization
     Color white, black, grey, pink, background;
+    Color orange, yellow, green;
     Image nyanCat, nyanCat_a;
 
+    initColor(&orange, "ff7800");
+    initColor(&yellow, "FFFF00");
+    initColor(&green, "00ff00");
     initColor(&white, "FFFFFF");
     initColor(&black, "000000");
     initColor(&grey, "77797A");
     initColor(&pink, "FF8AD1");
-    initColor(&background, "0C58D3");
+    initColor(&background, "0d1b46");
+
+
     openImage("nyancat", &nyanCat);
     openImage("nyancat_a", &nyanCat_a);
     topLeftX = fb.screen_width - 810;
@@ -52,6 +58,24 @@ int main(){
     x = nyanX;
     y = nyanY;
 
+
+    //Line matter
+    int lineInitX = botRightX - 410;
+    int lineInitY = botRightY - 50;
+
+    int line1x0 = lineInitX, line1y0 = lineInitY;
+    int line1x1 = lineInitX - 10;
+    int line1y1 = lineInitY - 10;
+
+    int line2x0 = lineInitX, line2y0 = lineInitY;
+    int line2x1 = lineInitX, line2y1 = lineInitY - 10;
+    
+    int line3x0 = lineInitX, line3y0 = lineInitY;
+    int line3x1 = lineInitX + 10, line3y1 = lineInitY - 10;
+    int timer = 0;
+    
+    int laser2 = 0;
+    int laser3 = 0;
     //Loop
     while(1){
         fillSquareArea(&fb, topLeftX, topLeftY,botRightX, botRightY, background);
@@ -76,7 +100,45 @@ int main(){
         }
         usleep(10000);
 
-        // drawThickLine(&fb, 400, 400, 600, 800, 10, white);
+        //Line matter
+        line1x0 -= 5;
+        line1y0 -= 5;
+        line1x1 -= 5;
+        line1y1 -= 5;
+        drawThickLine(&fb, line1x0, line1y0, line1x1, line1y1, 2, orange);
+        
+        laser2 = (line1x1 <= lineInitX - 30)? 1: 0;
+        if (laser2) {
+            line2y0 -= 5;
+            line2y1 -= 5;
+            drawThickLine(&fb, line2x0, line2y0, line2x1, line2y1, 3, yellow);
+        }
+
+        laser3 = (line2y1 <= lineInitY - 40)? 1: 0;
+        if (laser3) {
+            line3x0 += 5;
+            line3y0 -= 5;
+            line3x1 += 5;
+            line3y1 -= 5;
+            drawThickLine(&fb, line3x0, line3y0, line3x1, line3y1, 4, green);
+        }
+        
+        if (line1x1 <= topLeftX || line1y1 <= topLeftY + 6) {
+            line1x0 = lineInitX;
+            line1y0 = lineInitY;
+            line1x1 = lineInitX - 10;
+            line1y1 = lineInitY - 10;
+        }
+        if (line2y1 <= topLeftY + 6) {
+            line2y0 = lineInitY;
+            line2y1 = lineInitY - 10;    
+        }
+        if (line3x0 >= botRightX || line3y1 <= topLeftY + 6) {
+            line3x0 = lineInitX;
+            line3x1 = lineInitX + 10;
+            line3y0 = lineInitY;
+            line3y1 = lineInitY - 10;
+        }
     }
 
     updateFrame(&fb);
