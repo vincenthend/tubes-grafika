095cdc36be3c6b72a9f88a9dfbd6ef19d29ae2ad
rionaldichandraseta <rionaldi.chandraseta@gmail.com>
2018-02-03 23:36:56 +0700

    Added vertices struct, y minmax finder, todos

--- Tugas 3/rasterizer.c
+++ Tugas 3/rasterizer.c
@@ -1,2 +1,30 @@
 #include "rasterizer.h"
 
+int findMinX(Vertices vertices[], int vertex_count) {
+    int i;
+    int yMin = vertices[0]->v.y;
+    for (i=0; i<vertex_count; i++) {
+        if (vertices[i]->v.y < yMin) {
+            yMin = vertices[i]->v.y;
+        }
+    }
+    return yMin;
+}
+
+int findMaxY(Vertices vertices[], int vertex_count) {
+    int i;
+    int yMax = vertices[0]->v.y;
+    for (i=0; i<vertex_count; i++) {
+        if (vertices[i]->v.y > yMax) {
+            yMax = vertices[i]->v.y;
+        }
+    }
+}
+
+/**
+ * Follow https://www.cosc.brocku.ca/Offerings/3P98/course/lectures/2d/
+ * Scan converting solid polygons
+ */
+void simplePolygonFill(Vertex* vertices, int vertex_count, Color c) {
+
+}
--- Tugas 3/rasterizer.h
+++ Tugas 3/rasterizer.h
@@ -6,11 +6,20 @@
 #include "color.h"
 #include "graphics.h"
 
+#define MAX_VERTICES 25
+
 typedef struct {
     int x;
     int y;
 } Vertex;
 
+typedef struct {
+    Vertex v;
+} Vertices[MAX_VERTICES];
 
+/**
+ * A simple polygon is a polygon without any intersecting sides.
+ */
+void simplePolygonFill(Vertex* vertices, Color c);
 
 #endif
